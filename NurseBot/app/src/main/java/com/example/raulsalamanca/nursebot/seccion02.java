228bb70bfd5a79dee6f3c76542e263ffe974d13f
Raúl Salamanca <raul.salamanca@ad.yolandavilla.com>
2019-01-13 19:59:20 -0500

    correccion subida

---
+++ NurseBot/app/src/main/java/com/example/raulsalamanca/nursebot/seccion02.java
@@ -0,0 +1,16 @@
+package com.example.raulsalamanca.nursebot;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class seccion02 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seccion02, container, false);
+    }
+}
